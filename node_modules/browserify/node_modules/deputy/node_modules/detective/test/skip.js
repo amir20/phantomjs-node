27c782c6180da92cb1f20f79df044c8e519d948c
Amir Raminfar <findamir@gmail.com>
2012-12-11 21:50:54 -0500

    Fixes ignored files

--- node_modules/browserify/node_modules/deputy/node_modules/detective/test/skip.js
+++
@@ -1,9 +0,0 @@
-var test = require('tap').test;
-var detective = require('../');
-var fs = require('fs');
-var src = fs.readFileSync(__dirname + '/files/skip.js');
-
-test('skip', function (t) {
-    t.deepEqual(detective(src), [ 'a', 'b', 'c' ]);
-    t.end();
-});
--- node_modules/dnode-protocol/lib/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = Object.keys || function (obj) {
-    var keys = [];
-    for (var key in obj) keys.push(key);
-    return keys;
-};
