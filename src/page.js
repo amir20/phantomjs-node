3b347ffa7ceb73bfd13b298d5f413ca1399d794d
enjoythelive1 <enjoythelive1@gmail.com>
2016-04-28 16:57:49 -0400

    Made possible to set several event listeners to page events in phantom js and those events can be run in node runtime or phantom runtime

--- src/page.js
+++ src/page.js
@@ -6,6 +6,20 @@ export default class Page {
         this.target = 'page$' + pageId;
         this.phantom = phantom;
     }
+
+    on(event, runOnPhantom, callback) {
+        if (typeof runOnPhantom === 'function') {
+            let args = [].slice.call(arguments, 2);
+            return this.phantom.on(event, this.target, false, runOnPhantom.bind(this), args);
+        } else {
+            let args = [].slice.call(arguments, 3);
+            return this.phantom.on(event, this.target, runOnPhantom, runOnPhantom ? callback : callback.bind(this), args);
+        }
+    }
+
+    off(event) {
+        return this.phantom.off(event, this.target);
+    }
 }
 
 
--- src/shim.js
+++ src/shim.js
@@ -8,6 +8,26 @@ const objectSpace = {
     phantom: phantom
 };
 
+const events = {};
+
+const eventsForEntities = {
+    page: [
+        'onInitialized',
+        'onLoadStarted',
+        'onLoadFinished',
+        'onUrlChanged',
+        'onNavigationRequested',
+        'onRepaintRequested',
+        'onResourceRequested',
+        'onResourceReceived',
+        'onResourceError',
+        'onResourceTimeout',
+        'onAlert',
+        'onConsoleMessage',
+        'onClosing'
+    ]
+};
+
 /**
  * All methods that have a callback in their signature
  * @type {string[]}
@@ -33,11 +53,7 @@ const commands = {
                 // If the second parameter is a function then we want to proxy and pass parameters too
                 let callback = command.params[1];
                 let args = command.params.slice(2);
-                args.forEach(param => {
-                    if (param.target !== undefined) {
-                        objectSpace[param.target] = param;
-                    }
-                });
+                syncOutObjects(args);
                 objectSpace[command.target][command.params[0]] = function () {
                     let params = [].slice.call(arguments).concat(args);
                     return callback.apply(objectSpace[command.target], params);
@@ -122,6 +138,19 @@ function transform(object) {
 }
 
 /**
+ * Sync all OutObjects present in the array
+ *
+ * @param objects
+ */
+function syncOutObjects(objects) {
+    objects.forEach(param => {
+        if (param.target !== undefined) {
+            objectSpace[param.target] = param;
+        }
+    });
+}
+
+/**
  * Executes a command by first checking if it is a custom method and then calling the method on the target.
  * @param command the command to execute
  */
@@ -132,7 +161,13 @@ function executeCommand(command) {
         const target = objectSpace[command.target];
         const method = target[command.name];
 
-        if (haveCallbacks.indexOf(command.name) === -1) {
+        if (command.name === 'addEvent') {
+            addEvent(command.target, command.params[0].type, command.params[0].event, command.params[0].args);
+            completeCommand(command);
+        } else if (command.name === 'removeEvent') {
+            removeEvent(command.target, command.params[0].type);
+            completeCommand(command);
+        } else if (haveCallbacks.indexOf(command.name) === -1) {
             command.response = method.apply(target, command.params);
             completeCommand(command);
         } else {
@@ -147,6 +182,122 @@ function executeCommand(command) {
         throw new Error(`Cannot find ${command.name} method to execute on ${command.target} object.`);
     }
 }
+
+/**
+ * Adds an event to the target
+ *
+ * @param target the id of the object to listen
+ * @param eventName the name off the event
+ * @param callback the callback of the event
+ * @param args the additional arguments for the callback
+ */
+function addEvent(target, eventName, callback, args) {
+    let type = getTargetType(target);
+
+    if (isEventSupported(type, eventName)) {
+        let listeners = getEventListeners(target, eventName);
+
+        if (typeof callback === 'function') {
+            listeners.otherListeners.push(function () {
+                let params = [].slice.call(arguments).concat(args);
+                return callback.apply(objectSpace[target], params);
+            });
+        }
+    }
+}
+
+/**
+ * Removes all the listeners for an event in a target
+ *
+ * @param target the id of the target
+ * @param eventName the name of the event
+ */
+function removeEvent(target, eventName) {
+    let type = getTargetType(target);
+
+    if (isEventSupported(type, eventName)) {
+        events[target][eventName] = null;
+        objectSpace[target][eventName] = null;
+    }
+}
+
+/**
+ * Verifies if an event is supported for a type of target
+ *
+ * @param type
+ * @param eventName
+ * @returns {boolean}
+ */
+function isEventSupported(type, eventName) {
+    return eventsForEntities[type] && eventsForEntities[type].indexOf(eventName) !== -1;
+}
+
+/**
+ * Gets an object containing all the listeners for an event of a target
+ *
+ * @param target the target id
+ * @param eventName the event name
+ */
+function getEventListeners(target, eventName) {
+    if (!events[target]) {
+        events[target] = {};
+    }
+
+    if (!events[target][eventName]) {
+        events[target][eventName] = {
+            outsideListener: getOutsideListener(eventName, target),
+            otherListeners: []
+        };
+
+        objectSpace[target][eventName] = triggerEvent.bind(null, target, eventName);
+    }
+
+    return events[target][eventName];
+}
+
+/**
+ * Determines a targets type using its id
+ *
+ * @param target
+ * @returns {*}
+ */
+function getTargetType(target) {
+    return target.toString().split('$')[0];
+}
+
+/**
+ * Executes all the listeners for an event from a target
+ *
+ * @param target
+ * @param eventName
+ */
+function triggerEvent(target, eventName) {
+    let args = [].slice.call(arguments, 2);
+    let listeners = events[target][eventName];
+    listeners.outsideListener.apply(null, args);
+    listeners.otherListeners.forEach(function (listener) {
+        listener.apply(objectSpace[target], args);
+    });
+}
+
+/**
+ * Returns a function that will notify to node that an event have been triggered
+ *
+ * @param eventName
+ * @param targetId
+ * @returns {Function}
+ */
+function getOutsideListener(eventName, targetId) {
+    return function () {
+        var args = [].slice.call(arguments, 0);
+        system.stdout.writeLine('<event>' + JSON.stringify({
+            target: targetId,
+            type: eventName,
+            args
+        }));
+    };
+}
+
 
 /**
  * Completes a command by return a response to node and listening again for next command.
