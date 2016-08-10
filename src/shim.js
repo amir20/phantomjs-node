652af9de4b44aac262530b3988562d2a873a05df
Maciej Bogusz <maciej.bogusz@greatcare.pl>
2016-08-10 17:40:43 +0200

    shim.js: added Function.prototype.bind fallback for compatibility with PhantomJS 1.9 and earlier (used in getEventListeners()).

--- src/shim.js
+++ src/shim.js
@@ -2,6 +2,36 @@ import webpage from "webpage";
 import system from "system";
 
 /**
+ * Ensure that Function has bind() method (PhantomJS version <= 1.9 support)
+ * This is a Polyfill replacement from MDN.
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Polyfill}
+ */
+/* eslint-disable no-extend-native, consistent-this, require-jsdoc, no-empty-function, no-invalid-this */
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+        if (typeof this !== 'function') {
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+        }
+
+        let self = this;
+        let aArgs = Array.prototype.slice.call(arguments, 1);
+        function NoopFunction() {}
+        function boundFunction() {
+            return self.apply((this instanceof NoopFunction ? this : oThis), aArgs.concat(Array.prototype.slice.call(arguments)));
+        }
+
+        if (this.prototype) {
+            NoopFunction.prototype = this.prototype;
+        }
+        boundFunction.prototype = new NoopFunction();
+
+        return boundFunction;
+    };
+}
+
+/* eslint-enable no-extend-native, consistent-this, require-jsdoc, no-empty-function, no-invalid-this */
+
+/**
  * Stores all all pages and single instance of phantom
  */
 const objectSpace = {
