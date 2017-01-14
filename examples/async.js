2b39fb737f1c5a26a37fc92195faf4237d2decea
Amir Raminfar <findamir@gmail.com>
2017-01-14 07:55:59 -0800

    Adds Dockerfile to examples

--- examples/async.js
+++ examples/async.js
@@ -4,16 +4,14 @@ const phantom = require('phantom');
     const instance = await phantom.create();
     const page = await instance.createPage();
     await page.on("onResourceRequested", function(requestData) {
-        console.info('Requesting', data.url)
+        console.info('Requesting', requestData.url)
     });
 
-    const status = await page.open('https://stackoverflow.com/');
-    console.log(status);
-
+    const status = await page.open(process.argv[2]);
     const content = await page.property('content');
     console.log(content);
 
     await instance.exit();
 }());
 
-// node --harmony-async-await async.js
+// node --harmony-async-await async.js http://stackoverflow.com
