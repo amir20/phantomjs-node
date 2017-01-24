c8504c65d12b019f0beabdeda7e6fc6eaa957d6a
Amir Raminfar <araminfar@quid.com>
2017-01-24 09:49:48 -0800

    Adds more examples for render

---
+++ examples/render.js
@@ -0,0 +1,17 @@
+const phantom = require('phantom');
+
+(async function() {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+
+    await page.property('viewportSize', {width: 1024, height: 600});
+    const status = await page.open('https://stackoverflow.com/');
+    console.log(`Page opened with status [${status}].`);
+
+    await page.render('stackoverflow.pdf');
+    console.log(`File created at [./stackoverflow.pdf]`);
+
+    await instance.exit();
+}());
+
+// node --harmony-async-await render.js
